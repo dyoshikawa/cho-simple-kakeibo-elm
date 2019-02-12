c35003b19beb3bd3e950f611786a64b0affbd63d
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-02-12 11:27:24 +0900

    feat

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, h1, input, section, text)
-import Html.Attributes exposing (class, type_)
+import Html exposing (Html, button, div, h1, h2, input, section, text)
+import Html.Attributes exposing (class, placeholder, type_)
 import Html.Events exposing (onClick)
 
 
@@ -34,10 +34,27 @@ view model =
             ]
         , section [ class "section" ]
             [ div [ class "container" ]
+                [ h2
+                    [ class "subtitle" ]
+                    [ text "煩わしい入力項目のない超シンプルな家計簿です。" ]
+                ]
+            ]
+        , section [ class "section" ]
+            [ div [ class "container" ]
                 [ div [ class "field has-addons" ]
                     [ div [ class "control" ]
-                        [ input [ class "input", type_ "text" ] [] ]
+                        [ input [ class "input", type_ "text", placeholder "支出金額" ] [] ]
                     , div [ class "control" ] [ button [ class "button is-info" ] [ text "登録" ] ]
+                    ]
+                ]
+            ]
+        , section [ class "section" ]
+            [ div [ class "container" ]
+                [ div [ class "card" ]
+                    [ div [ class "card-content" ]
+                        [ div [ class "content" ]
+                            [ text "test" ]
+                        ]
                     ]
                 ]
             ]
