f9df30b37c27e4e9882136ca9c2831cb77d0b52e
yoshikawada@crunchtimer.jp <yoshikawada@crunchtimer.jp>
2019-02-16 03:20:55 +0900

    fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import moment from 'moment'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  const db = admin.firestore()
+  request.body.db.collection('items').add({
+    price: Number(request.body.price),
+    userUid: request.body.uid,
+    createdAt: moment().format('YYYY/MM/DD HH:mm:ss'),
+  })
+  response.send('Hello from Firebase!')
+})
