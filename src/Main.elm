33e096a5bd178140f43affbde10989808a7bb376
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-02-19 14:18:53 +0900

    fix

--- src/Main.elm
+++ src/Main.elm
@@ -256,6 +256,43 @@ viewInput t p v toMsg =
     input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
+spendView : String -> Bool -> List SpendItem -> (String -> msg) -> (String -> msg) -> (SpendItem -> msg) -> Html msg
+spendView spendInput spendBusy spendItems doneInput putSpendItem deleteSpendItem =
+    div []
+        [ section [ class "section" ]
+            [ div [ class "container" ]
+                [ div [ class "field has-addons" ]
+                    [ div [ class "control" ]
+                        [ input [ id "spendInput", class "input", type_ "number", placeholder "支出金額", onInput doneInput ] [] ]
+                    , div [ class "control" ]
+                        [ button
+                            [ class
+                                ((\busy ->
+                                    if busy == True then
+                                        "button is-info is-loading"
+
+                                    else
+                                        "button is-info"
+                                 )
+                                    spendBusy
+                                )
+                            , onClick (putSpendItem spendInput)
+                            ]
+                            [ text "登録" ]
+                        ]
+                    ]
+                ]
+            ]
+        , section [ class "section" ]
+            [ div [ class "container" ]
+                (spendItemCards
+                    spendItems
+                    deleteSpendItem
+                )
+            ]
+        ]
+
+
 spendItemCards : List SpendItem -> (SpendItem -> msg) -> List (Html msg)
 spendItemCards items msgDeletingItem =
     List.map
